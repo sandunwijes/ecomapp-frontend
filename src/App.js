e73f977f460a2a69498f7db4af5964a546744b52
sandunwijes <107408557+sandunwijes@users.noreply.github.com>
2022-12-07 17:02:49 +0530

    added single colans to paths

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Homescreens from "./screens/Homescreens";
 import { BrowserRouter, Route } from "react-router-dom";
 import Productdescscreen from "./screens/Productdescscreen";
-import React from 'react';
+import React from "react";
 import Cartscreen from "./screens/Cartscreen";
 import Loginscreen from "./screens/Loginscreen";
 import Registrationscreen from "./screens/Registrationscreen";
@@ -13,23 +13,21 @@ import Orderinfo from "./screens/Orderinfo";
 import Profilescreen from "./screens/Profilescreen";
 import Adminscreen from "./screens/Adminscreen";
 
-
 function App() {
   return (
     <div className="App">
       <Navbar />
 
       <BrowserRouter>
-        <Route path="/" component={Homescreens} exact/>
-        <Route path="/product/:id" component={Productdescscreen}/>
-        <Route path="/cart" component={Cartscreen}/>
-       <Route path="/login" component={Loginscreen}/>
-       <Route path="/register" component={Registrationscreen}/>
-       <Route path="/orders" component={Ordersscreen}/>
-       <Route path="/orderinfo/:orderid" component={Orderinfo}/>
-       <Route path="/profile" component={Profilescreen}/>
-       <Route path="/admin" component={Adminscreen}/>
-
+        <Route path='/' component={Homescreens} exact />
+        <Route path='/product/:id' component={Productdescscreen} />
+        <Route path='/cart' component={Cartscreen} />
+        <Route path='/login' component={Loginscreen} />
+        <Route path='/register' component={Registrationscreen} />
+        <Route path='/orders' component={Ordersscreen} />
+        <Route path='/orderinfo/:orderid' component={Orderinfo} />
+        <Route path='/profile' component={Profilescreen} />
+        <Route path='/admin' component={Adminscreen} />
       </BrowserRouter>
     </div>
   );
